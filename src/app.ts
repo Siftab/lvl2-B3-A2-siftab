bfa2ec24ca5be98d7b397c901819b2409e795ca6
Siftab <siiffuuu@gmail.com>
2024-06-29 17:45:47 +0600

    basic server setup and connection

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,3 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World! server hitted')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
